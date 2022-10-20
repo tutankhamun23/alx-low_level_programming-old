8900a002993c0d1f04cf031a34c6a18830f848c3
tutankhamun23 <gene.elvis10@email.com>
2022-10-20 15:52:16 -0700

    function that checks on uppercase characters

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Returns On success 1 else -1
+ */
+int _putchar(char c)
+	return (write(1, &c, 1));
